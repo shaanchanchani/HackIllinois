b633188e67f4a073188c7c36203d4c4138547cc4
Shaan Chanchani <shaanchanchani@wirelessprvnat-172-16-159-16.near.illinois.edu>
2023-02-24 19:13:03 -0600

    initial commit

---
+++ testing.py
@@ -0,0 +1,15 @@
+
+def main():
+    headers_dict = {"API_KEY": "PasteYourPersonalAPIKeyHere"}
+    URL = "API Endpoint"
+    wData = requests.get(url=URL, headers=headers_dict)
+
+    print()
+
+
+    """ Main program """
+    # Code goes over here.
+    return 0
+
+if __name__ == "__main__":
+    main()
